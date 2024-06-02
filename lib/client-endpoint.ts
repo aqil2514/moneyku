b432d2da2fa13df45c647c27120fd510da45aa34
Radjikin Septiawan <radjikinseptiawan@gmail.com>
2024-06-02 09:04:55 +0700

    auth debug

--- lib/client-endpoint.ts
+++ lib/client-endpoint.ts
@@ -2,4 +2,6 @@ const isLocal = process.env.NODE_ENV === "development";
 
 const clientEndpoint = isLocal ? String(process.env.LOCAL_WEB_ENDPOINT) : String(process.env.LOCAL_WEB_STAGGING);
 
+console.log(clientEndpoint);
+
 export default clientEndpoint
