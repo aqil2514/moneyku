96212a2d6c7eedf577735296cd58ce59468013f0
Muhamad Aqil Maulana <muhamadaqil383@gmail.com>
2024-07-31 02:58:34 +0700

    refactoring edit data transaction

--- app/routes/_main.transaction/Components/EditDataComponents.tsx
+++ app/routes/_main.transaction/Components/EditDataComponents.tsx
@@ -1,18 +1,46 @@
+import React, { useEffect, useState } from "react";
 import { useT_EditData } from "../Providers/EditDataProvider";
+import { rupiahConvert } from "utils/client/general";
+import { currencyFormat } from "utils/general";
+import Button from "components/Inputs/Button";
 
-export const MainId = () => {
-  const { globalData } = useT_EditData();
-  return <input type="hidden" name="main-id" defaultValue={globalData.id} />;
-};
-
-export const TransactionId = () => {
+export const AssetTransaction = ({
+  errorMessage,
+}: {
+  errorMessage?: string;
+}) => {
   const { selectedData } = useT_EditData();
   return (
-    <input
-      type="hidden"
-      name="transaction-uid"
-      defaultValue={selectedData?.uid}
-    />
+    <div className="form-text">
+      <label htmlFor="transaction-assets">Aset</label>
+      <input
+        type="text"
+        name="transaction-assets"
+        id="transaction-assets"
+        defaultValue={selectedData?.asset}
+      />
+      <em style={{ color: "red" }}>{errorMessage}</em>
+    </div>
+  );
+};
+
+export const CategoryTransaction = ({
+  errorMessage,
+}: {
+  errorMessage?: string;
+}) => {
+  const { priceChecked, selectedData } = useT_EditData();
+  return (
+    <div className="form-text">
+      <label htmlFor="transaction-category">Kategori {priceChecked}</label>
+      <input
+        type="text"
+        name="transaction-category"
+        id="transaction-category"
+        defaultValue={selectedData?.category}
+      />
+      <em style={{ color: "red" }}>{errorMessage}</em>
+    </div>
   );
 };
 
@@ -21,7 +49,7 @@ export const DateTransaction = ({
   errorMessage,
 }: {
   header: string;
-  errorMessage: string | undefined;
+  errorMessage?: string;
 }) => {
   const { formattedDate } = useT_EditData();
   return (
@@ -35,5 +63,119 @@ export const DateTransaction = ({
       />
       {errorMessage && <em style={{ color: "red" }}>{errorMessage}</em>}
     </div>
+  );
+};
+
+export const FooterButtons = ({
+  setEditPopup,
+}: {
+  setEditPopup: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <div className="popup-edit-footer" style={{ margin: "1rem 0" }}>
+      <div className="container container-flex">
+        <Button color="success">Edit Data</Button>
+
+        <Button color="error" type="button" onClick={() => setEditPopup(false)}>
+          Batal
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export const InOutCategory = ({ errorMessage }: { errorMessage?: string }) => {
+  const { priceChecked, setPriceChecked } = useT_EditData();
+  return (
+    <div className="form-navigation">
+      <input
+        type="radio"
+        name="transaction-type"
+        id="income-type"
+        value={"Pemasukan"}
+        onChange={() => setPriceChecked("Pemasukan")}
+        checked={priceChecked === "Pemasukan"}
+      />
+      <label htmlFor="income-type">Pemasukan</label>
+      <input
+        type="radio"
+        name="transaction-type"
+        id="outcome-type"
+        value={"Pengeluaran"}
+        onChange={() => setPriceChecked("Pengeluaran")}
+        checked={priceChecked === "Pengeluaran"}
+      />
+      <label htmlFor="outcome-type">Pengeluaran</label>
+      <em style={{ color: "red" }}>{errorMessage}</em>
+    </div>
+  );
+};
+
+export const MainId = () => {
+  const { globalData } = useT_EditData();
+  return <input type="hidden" name="main-id" defaultValue={globalData.id} />;
+};
+
+export const NominalTransaction = ({
+  errorMessage,
+}: {
+  errorMessage?: string;
+}) => {
+  const { selectedData } = useT_EditData();
+  const [inputValue, setInputValue] = useState<string>("");
+
+  useEffect(() => {
+    if (selectedData) {
+      setInputValue(currencyFormat.format(selectedData.price));
+    }
+  }, [selectedData]);
+
+  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    return rupiahConvert(e, setInputValue);
+  };
+
+  return (
+    <div className="form-text">
+      <label htmlFor="transaction-total">Total</label>
+      <input
+        type="text"
+        name="transaction-total"
+        id="transaction-total"
+        value={inputValue}
+        onChange={changeHandler}
+      />
+      <em style={{ color: "red" }}>{errorMessage}</em>
+    </div>
+  );
+};
+
+export const NoteTransaction = ({
+  errorMessage,
+}: {
+  errorMessage?: string;
+}) => {
+  const { selectedData } = useT_EditData();
+  return (
+    <div className="form-text">
+      <label htmlFor="transaction-note">Catatan</label>
+      <input
+        type="text"
+        name="transaction-note"
+        id="transaction-note"
+        defaultValue={selectedData?.item}
+      />
+      <em style={{ color: "red" }}>{errorMessage}</em>
+    </div>
+  );
+};
+
+export const TransactionId = () => {
+  const { selectedData } = useT_EditData();
+  return (
+    <input
+      type="hidden"
+      name="transaction-uid"
+      defaultValue={selectedData?.uid}
+    />
   );
 };
--- app/routes/_main.transaction/TransactionWithData/TWD_EditData.tsx
+++ app/routes/_main.transaction/TransactionWithData/TWD_EditData.tsx
@@ -3,12 +3,17 @@ import axios from "axios";
 import { useRevalidator } from "@remix-run/react";
 import { ErrorValidationResponse } from "~/@types/General";
 import { TransactionBodyType, TransactionType } from "~/@types/Transaction";
-import Button from "components/Inputs/Button";
 import { useTransactionData } from "../Transactions";
 import EditDataProvider from "../Providers/EditDataProvider";
 import {
+  AssetTransaction,
+  CategoryTransaction,
   DateTransaction,
+  FooterButtons,
+  InOutCategory,
   MainId,
+  NominalTransaction,
+  NoteTransaction,
   TransactionId,
 } from "../Components/EditDataComponents";
 import { getErrors } from "../utils-client";
@@ -19,184 +24,6 @@ interface EditPopupProps {
   setEditPopup: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
-// Ini berantakan nih. Rapihin nanti
-// export default function EditPopup({
-//   index,
-//   header,
-//   setEditPopup,
-// }: EditPopupProps) {
-//   const { data } = useTransactionData();
-//   const [globalData, setGlobalData] = useState<TransactionType>();
-//   const [selectedData, setSelectedData] = useState<TransactionBodyType>();
-//   const [priceChecked, setPriceChecked] = useState<
-//     "Pemasukan" | "Pengeluaran" | string
-//   >("");
-//   const [errors, setErrors] = useState<ErrorValidationResponse[]>([]);
-//   const revalidator = useRevalidator();
-//   const {
-//     dateTransaction,
-//     noteTransaction,
-//     typeTransaction,
-//     categoryTransaction,
-//     assetsTransaction,
-//     totalTransaction,
-//   } = getErrors(errors);
-
-//   const getData = useCallback(
-//     (header: string) => {
-//       const sameGlobal = data.find((d) => d.header === header);
-//       if (!sameGlobal)
-//         throw new Error("Terjadi kesalahan saat pengambilan data");
-//       setGlobalData(sameGlobal);
-//       setSelectedData(globalData?.body[index]);
-//       if (selectedData) {
-//         const category = selectedData.price > 0 ? "Pemasukan" : "Pengeluaran";
-
-//         setPriceChecked(category);
-//       }
-//     },
-//     [index, data, globalData, selectedData]
-//   );
-
-//   const formattedDate = (header: string) => {
-//     const date = new Date(header);
-
-//     const result = date.toISOString().split("T")[0];
-
-//     return result;
-//   };
-
-//   useEffect(() => {
-//     getData(header);
-//   }, [header, globalData, selectedData, getData]);
-
-//   const submitHandler = async (e: React.FormEvent<HTMLFormElement>) => {
-//     e.preventDefault();
-//     const formData = new FormData(e.target as HTMLFormElement);
-
-//     const res = await axios.putForm("/api/transaction", formData);
-//     const data = res.data.response;
-
-//     if (!data.success) {
-//       revalidator.revalidate();
-//       setErrors(data.data);
-//     } else {
-//       revalidator.revalidate();
-//       setEditPopup(false);
-//     }
-//   };
-
-//   return (
-//     <div className="popup">
-//       <div className="popup-edit">
-//         <div className="popup-edit-header">
-//           <h3>Edit Data</h3>
-//         </div>
-
-//         <div className="popup-edit-body">
-//           <form onSubmit={submitHandler}>
-//             <input type="hidden" name="main-id" defaultValue={globalData?.id} />
-//             <input
-//               type="hidden"
-//               name="transaction-uid"
-//               defaultValue={selectedData?.uid}
-//             />
-//             <div className="form-date">
-//               <label htmlFor="transaction-date">Tanggal Transaksi</label>
-//               <input
-//                 type="date"
-//                 name="transaction-date"
-//                 id="transaction-date"
-//                 defaultValue={formattedDate(header)}
-//               />
-//               <em style={{ color: "red" }}>{dateTransaction}</em>
-//             </div>
-
-//             <div className="form-navigation">
-//               <input
-//                 type="radio"
-//                 name="transaction-type"
-//                 id="income-type"
-//                 value={"Pemasukan"}
-//                 onChange={() => setPriceChecked("Pemasukan")}
-//                 checked={priceChecked === "Pemasukan"}
-//               />
-//               <label htmlFor="income-type">Pemasukan</label>
-//               <input
-//                 type="radio"
-//                 name="transaction-type"
-//                 id="outcome-type"
-//                 value={"Pengeluaran"}
-//                 onChange={() => setPriceChecked("Pengeluaran")}
-//                 checked={priceChecked === "Pengeluaran"}
-//               />
-//               <label htmlFor="outcome-type">Pengeluaran</label>
-//               <em style={{ color: "red" }}>{typeTransaction}</em>
-//             </div>
-
-//             <div className="form-text">
-//               <label htmlFor="transaction-total">Total</label>
-//               <input
-//                 type="text"
-//                 name="transaction-total"
-//                 id="transaction-total"
-//                 defaultValue={
-//                   selectedData && String(selectedData.price).replace("-", "")
-//                 }
-//               />
-//               <em style={{ color: "red" }}>{totalTransaction}</em>
-//             </div>
-//             <div className="form-text">
-//               <label htmlFor="transaction-category">
-//                 Kategori {priceChecked}
-//               </label>
-//               <input
-//                 type="text"
-//                 name="transaction-category"
-//                 id="transaction-category"
-//                 defaultValue={selectedData?.category}
-//               />
-//               <em style={{ color: "red" }}>{categoryTransaction}</em>
-//             </div>
-//             <div className="form-text">
-//               <label htmlFor="transaction-assets">Aset</label>
-//               <input
-//                 type="text"
-//                 name="transaction-assets"
-//                 id="transaction-assets"
-//                 defaultValue={selectedData?.asset}
-//               />
-//               <em style={{ color: "red" }}>{assetsTransaction}</em>
-//             </div>
-//             <div className="form-text">
-//               <label htmlFor="transaction-note">Catatan</label>
-//               <input
-//                 type="text"
-//                 name="transaction-note"
-//                 id="transaction-note"
-//                 defaultValue={selectedData?.item}
-//               />
-//               <em style={{ color: "red" }}>{noteTransaction}</em>
-//               <div className="popup-edit-footer" style={{ margin: "1rem 0" }}>
-//                 <div className="container container-flex">
-//                   <Button color="success">Edit Data</Button>
-
-//                   <Button
-//                     color="error"
-//                     type="button"
-//                     onClick={() => setEditPopup(false)}
-//                   >
-//                     Batal
-//                   </Button>
-//                 </div>
-//               </div>
-//             </div>
-//           </form>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
 export default function EditPopup({
   index,
   header,
@@ -205,6 +32,7 @@ export default function EditPopup({
   const { data } = useTransactionData();
   const [globalData, setGlobalData] = useState<TransactionType>();
   const [selectedData, setSelectedData] = useState<TransactionBodyType>();
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const [priceChecked, setPriceChecked] = useState<
     "Pemasukan" | "Pengeluaran" | string
   >("");
@@ -268,88 +96,13 @@ export default function EditPopup({
               <MainId />
               <TransactionId />
               <DateTransaction errorMessage={dateTransaction} header={header} />
+              <InOutCategory errorMessage={typeTransaction} />
+              <NominalTransaction errorMessage={totalTransaction} />
+              <CategoryTransaction errorMessage={categoryTransaction} />
+              <AssetTransaction errorMessage={assetsTransaction} />
+              <NoteTransaction errorMessage={noteTransaction} />
 
-              {/* Next lanjutin ini */}
-
-              <div className="form-navigation">
-                <input
-                  type="radio"
-                  name="transaction-type"
-                  id="income-type"
-                  value={"Pemasukan"}
-                  onChange={() => setPriceChecked("Pemasukan")}
-                  checked={priceChecked === "Pemasukan"}
-                />
-                <label htmlFor="income-type">Pemasukan</label>
-                <input
-                  type="radio"
-                  name="transaction-type"
-                  id="outcome-type"
-                  value={"Pengeluaran"}
-                  onChange={() => setPriceChecked("Pengeluaran")}
-                  checked={priceChecked === "Pengeluaran"}
-                />
-                <label htmlFor="outcome-type">Pengeluaran</label>
-                <em style={{ color: "red" }}>{typeTransaction}</em>
-              </div>
-
-              <div className="form-text">
-                <label htmlFor="transaction-total">Total</label>
-                <input
-                  type="text"
-                  name="transaction-total"
-                  id="transaction-total"
-                  defaultValue={
-                    selectedData && String(selectedData.price).replace("-", "")
-                  }
-                />
-                <em style={{ color: "red" }}>{totalTransaction}</em>
-              </div>
-              <div className="form-text">
-                <label htmlFor="transaction-category">
-                  Kategori {priceChecked}
-                </label>
-                <input
-                  type="text"
-                  name="transaction-category"
-                  id="transaction-category"
-                  defaultValue={selectedData?.category}
-                />
-                <em style={{ color: "red" }}>{categoryTransaction}</em>
-              </div>
-              <div className="form-text">
-                <label htmlFor="transaction-assets">Aset</label>
-                <input
-                  type="text"
-                  name="transaction-assets"
-                  id="transaction-assets"
-                  defaultValue={selectedData?.asset}
-                />
-                <em style={{ color: "red" }}>{assetsTransaction}</em>
-              </div>
-              <div className="form-text">
-                <label htmlFor="transaction-note">Catatan</label>
-                <input
-                  type="text"
-                  name="transaction-note"
-                  id="transaction-note"
-                  defaultValue={selectedData?.item}
-                />
-                <em style={{ color: "red" }}>{noteTransaction}</em>
-                <div className="popup-edit-footer" style={{ margin: "1rem 0" }}>
-                  <div className="container container-flex">
-                    <Button color="success">Edit Data</Button>
-
-                    <Button
-                      color="error"
-                      type="button"
-                      onClick={() => setEditPopup(false)}
-                    >
-                      Batal
-                    </Button>
-                  </div>
-                </div>
-              </div>
+              <FooterButtons setEditPopup={setEditPopup} />
             </form>
           </div>
         </div>
@@ -357,18 +110,3 @@ export default function EditPopup({
     </EditDataProvider>
   );
 }
-
-// function getErrors(errors: ErrorValidationResponse[]) {
-//   const errorMap = new Map<keyof TransactionErrors, string>(
-//     errors.map((e) => [e.path as keyof TransactionErrors, e.message])
-//   );
-
-//   const error: TransactionErrors = {
-//     dateTransaction: errorMap.get("dateTransaction"),
-//     typeTransaction: errorMap.get("typeTransaction"),
-//     categoryTransaction: errorMap.get("categoryTransaction"),
-//     assetsTransaction: errorMap.get("assetsTransaction"),
-//     noteTransaction: errorMap.get("noteTransaction"),
-//   };
-//   return error;
-// }
